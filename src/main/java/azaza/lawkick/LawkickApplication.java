ca6d478a14bfccb0d05157c6cf097ad03c089455
Gyuhyeok <ghkdrbgur13@naver.com>
2023-11-04 13:22:14 +0900

    feat : 2023 이노씽크 메이커톤 프로젝트 초기 설정

---
+++ src/main/java/azaza/lawkick/LawkickApplication.java
@@ -0,0 +1,13 @@
+package azaza.lawkick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawkickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawkickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/azaza/lawkick/LawkickApplicationTests.java
@@ -0,0 +1,13 @@
+package azaza.lawkick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawkickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
