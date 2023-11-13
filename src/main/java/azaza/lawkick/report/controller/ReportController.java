7a51db28883962d4608a74b4fd789dc6398c2e75
Gyuhyeok <ghkdrbgur13@naver.com>
2023-11-13 17:41:18 +0900

    test : 예외처리 작동 테스트

--- src/main/java/azaza/lawkick/report/controller/ReportController.java
+++ src/main/java/azaza/lawkick/report/controller/ReportController.java
@@ -5,6 +5,7 @@ import azaza.lawkick.report.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -14,10 +15,10 @@ public class ReportController {
 
     private final ReportService reportService;
 
-    @GetMapping
-    public BaseResponse<String> testAPI(){
-
-        return BaseResponse.onSuccess("OK");
+    //예외처리 테스트
+    @GetMapping("/exception")
+    public BaseResponse<Object> exceptionAPI(@RequestParam Integer flag){
+        reportService.CheckFlag(flag);
+        return BaseResponse.onSuccess(flag);
     }
-
 }
--- src/main/java/azaza/lawkick/report/service/ReportService.java
+++ src/main/java/azaza/lawkick/report/service/ReportService.java
@@ -1,5 +1,7 @@
 package azaza.lawkick.report.service;
 
+import azaza.lawkick.config.code.status.ErrorStatus;
+import azaza.lawkick.config.exception.handler.TempHandler;
 import azaza.lawkick.report.repository.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +11,10 @@ import org.springframework.stereotype.Service;
 public class ReportService {
 
     private final ReportRepository reportRepository;
+
+    public void CheckFlag(Integer flag) {
+        if (flag == 1)
+            throw new TempHandler(ErrorStatus.TEMP_EXCEPTION);
+    }
+
 }
