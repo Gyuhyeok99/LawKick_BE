94f45309477fdf9d4dbd1b9abeeb2955edb2f525
Gyuhyeok <ghkdrbgur13@naver.com>
2023-11-14 14:09:01 +0900

    feat : BaseTimeEntity 적용을 위해 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/azaza/lawkick/LawkickApplication.java
+++ src/main/java/azaza/lawkick/LawkickApplication.java
@@ -2,8 +2,10 @@ package azaza.lawkick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LawkickApplication {
 
 	public static void main(String[] args) {
