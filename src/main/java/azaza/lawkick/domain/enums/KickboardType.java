fe815df918d6bff212ddb48173f791d05a766d35
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2023-11-15 21:33:36 +0900

    chore: KickboardType 일부 수정

--- src/main/java/azaza/lawkick/domain/enums/KickboardType.java
+++ src/main/java/azaza/lawkick/domain/enums/KickboardType.java
@@ -8,9 +8,10 @@ import lombok.RequiredArgsConstructor;
 public enum KickboardType implements EnumModel{
     SINGSING("씽씽")
     ,GCOO("지쿠")
-    ,LIME("라임")
+    ,BEAM("빔")
     ,KICKGOING("킥고잉")
-    ,ALPACA("알파카")
+    ,DEER("디어")
+    ,SWING("스윙")
     ;
 
     private final String value;
