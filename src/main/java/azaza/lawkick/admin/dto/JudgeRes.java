5ca484f3d39fab571b85407aed25c64c90b5e3d3
Gyuhyeok <ghkdrbgur13@naver.com>
2023-11-17 18:10:57 +0900

    feat : 관리자페이지에서 최종 신고 완료 api 추가

---
+++ src/main/java/azaza/lawkick/admin/dto/JudgeRes.java
@@ -0,0 +1,13 @@
+package azaza.lawkick.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class JudgeRes {
+
+    private Long reportId;
+}
--- src/main/java/azaza/lawkick/admin/service/AdminService.java
+++ src/main/java/azaza/lawkick/admin/service/AdminService.java
@@ -2,6 +2,8 @@ package azaza.lawkick.admin.service;
 
 
 import azaza.lawkick.admin.dto.AdminRes;
+import azaza.lawkick.admin.dto.JudgeRes;
+import azaza.lawkick.config.exception.handler.ReportHandler;
 import azaza.lawkick.domain.Report;
 import azaza.lawkick.domain.enums.ReportStatus;
 import azaza.lawkick.report.repository.ReportRepository;
@@ -12,6 +14,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static azaza.lawkick.config.code.status.ErrorStatus.INVALID_REPORT_ID;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -29,6 +33,17 @@ public class AdminService {
             adminRes.add(new AdminRes(report));
         }
         return adminRes;
+    }
+
+    @Transactional
+    public JudgeRes reportTrue(Long reportId) {
+        Report findReport = reportRepository.findById(reportId)
+                .orElseThrow(() -> new ReportHandler(INVALID_REPORT_ID));
+        findReport.updateReportTrue();
+        log.info("최종 신고 완료로 마일리지 적립 {}", findReport.getReportStatus());
+        //마일리지 적립 기능 추가해야함
+        return new JudgeRes(reportId);
+
     }
 
 }
