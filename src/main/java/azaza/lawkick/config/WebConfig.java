2f51f8b4023a6655066e611bc4605f44577a6a67
Gyuhyeok <ghkdrbgur13@naver.com>
2023-11-17 22:44:26 +0900

    bug : cors 문제 해결 못해서 일단 @CrossOrigin으로 다 열어둠

--- src/main/java/azaza/lawkick/config/WebConfig.java
+++ src/main/java/azaza/lawkick/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
@@ -13,4 +14,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PATCH")
                 .maxAge(3000);
     }
-}
+}*/
--- src/main/java/azaza/lawkick/member/controller/MemberController.java
+++ src/main/java/azaza/lawkick/member/controller/MemberController.java
@@ -3,12 +3,14 @@ package azaza.lawkick.member.controller;
 
 import azaza.lawkick.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/members")
+@CrossOrigin
 public class MemberController {
 
     private final MemberService memberService;
