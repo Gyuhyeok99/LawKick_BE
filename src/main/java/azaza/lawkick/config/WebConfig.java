364c35f1e9e8d835b99c1361ffd2fe5ec03e4c3c
Gyuhyeok <ghkdrbgur13@naver.com>
2023-11-17 18:38:28 +0900

    feat : cors 문제 해결 추가

---
+++ src/main/java/azaza/lawkick/config/WebConfig.java
@@ -0,0 +1,16 @@
+package azaza.lawkick.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://13.209.203.240:8080")
+                .allowedMethods("GET", "POST", "PATCH")
+                .maxAge(3000);
+    }
+}
